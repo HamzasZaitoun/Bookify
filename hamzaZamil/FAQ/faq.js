12bf392f69f22172b80fb0cf541dae1540af2f39
HamzaZamil <hamzaalzamil@gmail.com>
2024-10-03 21:26:17 +0300

    Fixing Bugs

--- hamzaZamil/FAQ/faq.js
+++ hamzaZamil/FAQ/faq.js
@@ -7,3 +7,13 @@ function hideAnswer(questionNumber) {
   const answerElement = document.getElementById(`faq-answer-${questionNumber}`);
   answerElement.style.display = "none";
 }
+
+function showAnswer(questionNumber) {
+  const answerElement = document.getElementById(`faq-answer-${questionNumber}`);
+  answerElement.classList.add("show");
+}
+
+function hideAnswer(questionNumber) {
+  const answerElement = document.getElementById(`faq-answer-${questionNumber}`);
+  answerElement.classList.remove("show");
+}
