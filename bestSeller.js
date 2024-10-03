2c7ec7e74c7122e61b63eaef42a1f85793f41248
Razanhroub <razan.b.alhroub@gmail.com>
2024-10-03 10:29:02 +0300

    razan getting pull

---
+++ bestSeller.js
@@ -0,0 +1,15 @@
+"use strict"
+	fetch('AsmaMarar/books.json')
+		.then(response => response.json())
+		.then(data => {
+			const book = data.books[3];  // Get only the 4th book (index 3)
+
+			// Update the content with the necessary info
+			document.getElementById("titleH2").textContent = book.title;
+			document.getElementById("author-name").textContent = "By " + book.author;
+			document.getElementById("main-description").textContent = book.description;
+			document.getElementById("price").textContent = "$ " + book.price;
+			document.getElementById("bookImage").src = "AsmaMarar/" + book.image_url; // Ensure this is correct based on your JSON
+		})
+		.catch(error => console.error('Error loading JSON:', error));
+
